ef049c08b17f684fa46cc8a7c80c6bc9b0e8ec66
pavlo91 <pavlofetisov289@gmail.com>
2023-12-05 21:11:51 -0700

    Added Slider component.

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,18 +1,5 @@
 <template>
-    <div class="grid lg:grid-cols-2 justify-items-center lg:justify-items-start lg:gap-14 xl:gap-20 mb-8 md:mb-12 lg:mb-16 xl:mb-20 px-5 md:px-10 lg:px-20 xl:px-40">
-        <div class="lg:hidden">
-            <img src="../assets/images/illustration-intro.svg" alt="" id="image" class="w-[45rem]">
-        </div>
-        <div class="max-w-lg box text-center lg:text-left flex flex-col items-center lg:items-start">
-            <h3 class="text-primary-DarkBlue font-bold text-5xl md:text-6xl lg:text-5xl xl:text-6xl leading-snug lg:leading-[1.15] xl:leading-[1.15] mb-10">Bring everyone together to build better products.</h3>
-            <p class="lg:text-lg md:text-2xl text-xl lg:max-w-sm mb-10">Manage makes it simple for software teams to plan day-to-day 
-            tasks while keeping the larger team goals in view.</p>
-            <MainButton text="Get Started" class="mb-5 drop" />
-        </div>
-        <div class="lg:block hidden">
-            <img src="../assets/images/illustration-intro.svg" alt="" id="image">
-        </div>
-    </div>
+    <Slider />
     <div class="grid grid-flow-row benefits lg:grid-cols-2 lg:gap-14 xl:gap-20 my-8 mb:my-12 lg:my-16 xl:my-20 lg:px-20 xl:px-40 relative">
        <div class="max-w-lg box2 text-center lg:text-left m-auto w-full lg:my-0">
         <h4 class="text-primary-DarkBlue font-bold lg:mt-40 text-4xl md:text-[2.75rem] lg:text-4xl xl:text-[2.75rem] mb-10">What’s different about Manage?</h4>
@@ -54,6 +41,7 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import MainButton from './MainButton.vue';
+import Slider from './Slider.vue';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,10 +1,5 @@
 <template>
-    <div class="grid lg:grid-cols-2 gap-5 justify-items-center bg-primary-BrightRed items-center py-20 relative overflow-hidden px-5 md:px-10 lg:px-20 xl:px-40">
-        <img src="../assets/images/bg-tablet-pattern.svg" alt="" class="absolute mix-blend-soft-light opacity-25 lg:-bottom-[15rem] xl:-bottom-[25rem] lg:left-8 xl:left-20 lg:w-[35rem] xl:w-[45rem]">
-        <h4 class="text-secondary-LightGray text-center lg:text-left font-bold text-4xl xl:text-[2.75rem]">Simplify how your team works today.</h4>
-        <button class="p-4 xl:p-5 px-8 xl:px-10 rounded-full font-bold text-primary-BrightRed bg-secondary-LightGray shadow-lg w-fit h-fit lg:justify-self-end">Get Started</button>
-        <img src="../assets/images/bg-tablet-pattern.svg" alt="" class="absolute mix-blend-soft-light opacity-25 lg:-right-[28rem] lg:-top-[31rem] xl:-right-[40rem] xl:-top-[38rem] lg:w-[40rem] xl:w-[51rem]">
-    </div>
+    
     <div class="grid lg:grid-flow-col bg-secondary-DarkBlue text-secondary-LightGray pt-10 xl:pt-20 pb-7 xl:pb-10 px-5 md:px-10 lg:px-20 xl:px-40">
         <div class="grid">
             <img src="../assets/images/logo.svg" alt="" class="self-start">

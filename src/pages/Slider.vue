3e5d8a19d96ac5f160b2645415fe07872d7bf9be
pavlo91 <pavlofetisov289@gmail.com>
2023-12-08 07:15:44 -0700

    Added slogan animation.

--- src/pages/Slider.vue
+++ src/pages/Slider.vue
@@ -10,10 +10,16 @@
         <div class="lg:hidden flex mx-4" id="image">
             <ImageItem url="src/assets/images/Events.svg" class-name="z-10" img-class="w-[45rem]" />
             <ImageItem url="src/assets/images/Tickets.svg" class-name="mt-14 -ml-10" img-class="w-[45rem]" />
-        </div>
-        <div class="flex slogan-bg items-center gap-14 lg:gap-8 lg:py-12 overflow-hidden md:px-10 lg:px-20 xl:px-40 lg:absolute lg:-bottom-12 md:w-[120%] z-20">
-          <p class="ml-10 lg:ml-0 solution-description">Our partner</p>
-          <div class="flex lg:gap-4 xl:gap-8">
+          </div>
+          <div class="slogan-bg items-center gap-14 lg:gap-8 lg:py-12 overflow-hidden md:px-10 lg:px-20 xl:px-40 lg:absolute lg:-bottom-12 md:w-[120%] z-20">
+          <!-- <p class="ml-10 lg:ml-0 solution-description">Our partner</p> -->
+          <div class="marque grid grid-flow-col relative" id="slogan">
+              <img src="../assets/images/swin-logo-gray.svg" alt="Swin Logo">
+              <img src="../assets/images/swin-logo-gray.svg" alt="Swin Logo">
+              <img src="../assets/images/swin-logo-gray.svg" alt="Swin Logo">
+              <img src="../assets/images/swin-logo-gray.svg" alt="Swin Logo">
+              <img src="../assets/images/swin-logo-gray.svg" alt="Swin Logo">
+              <img src="../assets/images/swin-logo-gray.svg" alt="Swin Logo">
               <img src="../assets/images/swin-logo-gray.svg" alt="Swin Logo">
           </div>
         </div>
@@ -21,7 +27,26 @@
 </template>
 
 <script setup lang="ts">
+  import { onMounted } from 'vue';
   import ItemWithButton from '@/components/ItemWithButton.vue';
   import ImageItem from '@/components/ImageItem.vue';
   import MainButton from '@/components/MainButton.vue';
+  onMounted(() => {
+        slideSlogan()
+    })
+  const slideSlogan = () => {
+      let arr = <any>[]
+      for (let i = 0; i < 7; i++) {
+          arr.push(document.getElementById('slogan')?.children.item(i))
+      }
+      console.log(arr)
+      let i = 1
+      setInterval(() => {
+          // arr.forEach()
+          arr.forEach((e: any) => {
+              e.style.transform = `translateX(-${i * 5}%)`;
+          });
+          i < arr.length ? i++ : i = 0
+      }, 1000)
+  }
 </script>
